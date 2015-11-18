d1530c7a29099841f0748aefad7dbab7a72e7a1c
Serviox19 <servio19@gmail.com>
2015-11-17 21:09:15 -0500

    made delete buttons work

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 
-var row = document.getElementById(rowid);
+var removeRow = document.getElementsByClassName('delete');
+
+var table = document.getElementById('myTable');
 
 
+function getOut () {
+  this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
+};
+
+//Make delete buttons array
+for (var i = 0; i < removeRow.length; i++) {
+  removeRow[i].addEventListener('click',getOut);
+};
